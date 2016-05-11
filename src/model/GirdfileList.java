0a177f452d8d3d27dfc28a8fb18da7fa598f6bca
gethobby <iamachineseman@sina.cn>
2016-05-11 10:24:40 +0800

    NodesoftManager第一次提交

---
+++ src/model/GirdfileList.java
@@ -0,0 +1,153 @@
+﻿package model;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+
+public class GirdfileList {
+
+	String checkedFileSql="select ObjectID,目标名称,一级类别,二级类别 ,所属国家 "
+			+ "from geomodel.objectinfo,geomodel.ObjectType_view "
+			+ "where 目标分类=geomodel.ObjectType_view.ID";
+//	String unCheckedFileSql="select ID,filename from gridfile where verified=0;";
+//	String searchFileSql="select ID,filename from gridfile where filename LIKE ?;";
+//	String RecentModelFileSql="SELECT ID,filename FROM gridfile limit 0,3; ";
+//	String ProjectSql="SELECT computetaskId,taskname,time,status,solver FROM computetask; ";
+//	String NodemachineSql="SELECT * from nodeMachine";
+	String UserlistSql="select userID,username,role,status from platformuser.account;";
+	String SoftlistSql="select * from objectmodelingsoft.softinfo";
+	ArrayList<String> arrayList1;
+	ArrayList<String[]> arrayList2;
+	/*
+	 * getSoftList
+	 * @return    返回数据库中现存的所有模型信息记录
+	 */
+	public ArrayList<String[]> getObjectModelList()
+	{
+		arrayList1 = new ArrayList<String>();
+		arrayList2 = new ArrayList<String[]>();
+		ArrayList<String[]> list = null;
+		mySQLConnector con=new mySQLConnector();
+		ResultSet rs=con.executeQuery(checkedFileSql);
+		try
+		{		
+			convertList(rs);
+			list=arrayList2;
+		}catch(Exception e)
+		{ 
+			e.printStackTrace();
+			}
+		con.close();
+		return list;
+	}
+	/*
+	 * getUserList
+	 * @return    返回数据库中现存的所有用户信息记录
+	 */
+	public ArrayList<String[]> getUserList()
+	{
+		arrayList1 = new ArrayList<String>();
+		arrayList2 = new ArrayList<String[]>();
+		ArrayList<String[]> list = null;
+		mySQLConnector con=new mySQLConnector();
+		ResultSet rs=con.executeQuery(UserlistSql);
+		try
+		{		
+			convertList(rs);
+			list=arrayList2;
+		}catch(Exception e)
+		{ 
+			e.printStackTrace();
+			}
+		con.close();
+		return list;
+	}
+	/*
+	 * getNodeMachineList
+	 * @return    返回数据库中现存的所有节点机信息记录
+	 */
+	public ArrayList<String[]> getNodeMachineList()
+	{
+		arrayList1 = new ArrayList<String>();
+		arrayList2 = new ArrayList<String[]>();
+		ArrayList<String[]> list = null;
+//		mySQLConnector con=new mySQLConnector();
+//		ResultSet rs=con.executeQuery(NodemachineSql);
+//		try
+//		{		
+//			convertList(rs);
+//			list=arrayList2;
+//		}catch(Exception e)
+//		{ 
+//			e.printStackTrace();
+//			}
+//		con.close();
+		return list;
+	}
+	/*
+	 * convertList
+	 * 提取ResultSet数据并将数据转化成ArrayList类型
+	 * @param    ResulrSet类型数据，待转化的数据库查询结果
+	 * @exception  可能抛出数据库访问异常SQLException
+	 */
+	public  void convertList(ResultSet rs) throws SQLException {
+
+		//List list = new ArrayList();
+
+		ResultSetMetaData md = rs.getMetaData();
+
+		int columnCount = md.getColumnCount(); // Map rowData;
+
+		while (rs.next()){ // rowData = new HashMap(columnCount);
+			//Map rowData = new HashMap();
+			String[] temp=new String[columnCount];
+			for (int i = 1; i <= columnCount; i++) {
+				arrayList1.add(md.getColumnName(i));
+				temp[i-1]=rs.getString(i);
+				//rowData.put(md.getColumnName(i), rs.getObject(i));
+			}
+
+			arrayList2.add(temp);
+
+		} 
+		//return list;
+
+	}
+	
+	/*
+	 * getSoftList
+	 * @return    返回数据库中现存的所有软件记录
+	 */
+	public ArrayList<String[]> getSoftList()
+	{
+		arrayList1 = new ArrayList<String>();
+		arrayList2 = new ArrayList<String[]>();
+		ArrayList<String[]> list = null;
+		mySQLConnector con=new mySQLConnector();
+		ResultSet rs=con.executeQuery(SoftlistSql);
+		try
+		{		
+			convertList(rs);
+			list=arrayList2;
+		}catch(Exception e)
+		{ 
+			e.printStackTrace();
+			}
+		con.close();
+		return list;
+	}
+
+	//下面为测试代码
+//	public static void main(String[] args)
+//	{
+//		GirdfileList gf=new GirdfileList();
+//		ArrayList<String[]> list=gf.getFileList();
+//		if(list!=null){
+//			System.out.println(list.get(1));
+//		}
+//		
+//	}
+}
---
+++ src/model/mySQLConnector.java
@@ -0,0 +1,221 @@
+﻿package model;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
+import com.sun.tracing.dtrace.ArgsAttributes;
+
+import developConfig.Gobal;
+
+
+public class mySQLConnector {
+		
+		/*以下是定义一些字段*/
+		//private String url = "jdbc:mysql://localhost:3306/model3d?user=root&password=blue";	//数据库URL
+		private static String className = "com.mysql.jdbc.Driver";	//数据库驱动类路径
+		private Connection conn = null;	//声明一个Connection对象
+		private Statement stmt = null;	//声明一个Statement对象
+		private PreparedStatement pstmt = null;	//声明一个PreparedStatement对象
+		
+		/*初始化一些东西：加载数据库驱动，并建立连接*/
+		public void init(){
+			try {
+				//加载数据库驱动
+				Class.forName("com.mysql.jdbc.Driver");
+				//System.out.println("driver load success");
+				//建立连接，连接到由属性url指定的数据库URL，并指定登录数据库的用户名和密码
+				conn = DriverManager.getConnection(Gobal.QUERY_DB_URL,Gobal.QUERY_DB_USER,Gobal.QUERY_DB_PASSWORD);
+				//System.out.println("connect success");
+				//在初始化方法里面首先获取Statement对象
+				stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY );
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("初始化方法init()出现错误，加载数据库驱动失败 或者 建立连接失败！！");
+			}
+		}
+		
+		/*构造方法*/
+		public mySQLConnector(){
+			init();
+		}
+/*test code		public static void main(String args[])
+		{
+			mySQLConnector con=new mySQLConnector();
+			ResultSet rs=con.executeQuery("select * from USER_ACCOUNT;");
+			try{
+			if(rs.next())
+				System.out.println("success");}
+			catch(Exception e){e.printStackTrace();}
+		}
+*/
+		/*********************************以下方法是针对Statement的****************************************/
+		
+		/*查询数据库，返回结果集ResultSet*/
+		public ResultSet executeQuery(String sql){
+			ResultSet rs = null;
+			try {
+				rs = stmt.executeQuery(sql);
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("executeQuery(String sql)方法出错！！");
+			}
+			return rs;
+		}
+		
+		/*对数据库中的数据进行增加、修改或删除，返回受影响的行数*/
+		public int executeUpdate(String sql){
+			int iCount = 0;
+			try {
+				iCount = stmt.executeUpdate(sql);
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("executeUpdate(String sql)方法出错！！");
+			}
+			return iCount;
+		}
+		
+		/*********************************以下方法是针对PreparedStatement的****************************/
+		
+		/*依据SQL语句给pstmt赋上值*/
+		public void readyPreparedStatement(String sql){
+			try {
+				pstmt =  conn.prepareStatement(sql,ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY );
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("readyPreparedStatement()方法出错！！");
+			}
+		}
+		
+		/*以下是一些 PreparedStatement 设置的方法*/
+		public void setString(int index, String value){
+			try {
+				if(value.trim().equals("")) pstmt.setNull(index, index);
+				else pstmt.setString(index, value);
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("PreparedStatement的设置方法setString()出错！！");
+			}
+		}
+		public void setInt(int index, int value){
+			try {
+				pstmt.setInt(index, value);
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("PreparedStatement的设置方法setInt()出错！！");
+			}
+		}
+		public void setBoolean(int index, boolean value){
+			try {
+				pstmt.setBoolean(index, value);
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("PreparedStatement的设置方法setBoolean()出错！！");
+			}
+		}
+		public void setDate(int index, Date value){
+			try {
+				pstmt.setDate(index, value);
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("PreparedStatement的设置方法setDate()出错！！");
+			}
+		}
+		public void setLong(int index, long value){
+			try {
+				pstmt.setLong(index, value);
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("PreparedStatement的设置方法setLong()出错！！");
+			}
+		}
+		public void setFloat(int index, float value){
+			try {
+				pstmt.setFloat(index, value);
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("PreparedStatement的设置方法setFloat()出错！！");
+			}
+		}
+		public void setBytes(int index, byte[] value){
+			try {
+				pstmt.setBytes(index, value);
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("PreparedStatement的设置方法setBytes()出错！！");
+			}
+		}
+		public void setTimeStamp(int index,Timestamp value){
+			try {
+				pstmt.setTimestamp(index, value);
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("PreparedStatement的设置方法setTimeStamp()出错！！");
+			}
+		}
+		
+		/*查询数据库，返回结果集ResultSet*/
+		public ResultSet executeQuery(){
+			if (pstmt != null) {
+				try {
+					return pstmt.executeQuery();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					System.out.println("executeQuery()方法出错！！");
+					return null;
+				}
+			} else{
+				System.out.println("PreparedStatement 未给定值！！");
+				return null;
+			}
+		}
+		
+		/*对数据库中的数据进行增加、修改或删除，返回受影响的行数*/
+		public int executeUpdate(){
+			int iCount = 0;
+			if (pstmt != null){
+				try {
+					iCount = pstmt.executeUpdate();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					System.out.println("executeUpdate()方法出错！！");
+				}
+			} else{
+				System.out.println("PreparedStatement 未给定值！！");
+			}
+			return iCount;
+		}
+		
+		/*********************************************************************************************************/
+		
+		/*关闭所有连接*/
+		public void close(){
+			try {
+				if (stmt != null) {
+					stmt.close();
+					stmt = null;
+				}
+				if (pstmt != null) {
+					pstmt.close();
+					pstmt = null;
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("关闭所有连接的方法close()出错了！！");
+			}
+		}
+		
+		protected void finalize()
+		{
+			this.close();
+		}
+
+}
